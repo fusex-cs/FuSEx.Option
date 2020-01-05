ec8b1b9ba8a4d7293477ddbee787010acc63fa03
William Taylor <38103171+wtayl0r@users.noreply.github.com>
2020-01-05 16:28:09 +0000

    Documentation (#4)

--- FuSEx.Option/Extensions/EnumerableExtensions.cs
+++ FuSEx.Option/Extensions/EnumerableExtensions.cs
@@ -6,32 +6,75 @@ namespace FuSEx.Option.Extensions
 {
     public static class EnumerableExtensions
     {
+        /// <summary>
+        /// Gets the first item in a sequence wrapped in an Option
+        /// </summary>
+        /// <returns>
+        /// <c>Some</c> with the first item of the sequence if the sequence has at least one item in it. <br/>
+        /// <c>None</c> if the sequence is empty.
+        /// </returns>
         public static Option<T> FirstOrNone<T>(this IEnumerable<T> sequence) =>
             sequence.Select(x => (Option<T>)new Some<T>(x))
                 .DefaultIfEmpty(None.Value)
                 .First();
 
+        /// <summary>
+        /// Gets the first item in a sequence that matches the supplied predicate, wrapped in an Option
+        /// </summary>
+        /// <param name="predicate">The predicate to filter the list with.</param>
+        /// <returns>
+        /// <c>Some</c> with the first item of the sequence if the sequence has at least one item in it that matches the predicate. <br/>
+        /// <c>None</c> if no items in the sequence match the predicate.
+        /// </returns>
         public static Option<T> FirstOrNone<T>(
             this IEnumerable<T> sequence,
             Func<T, bool> predicate) =>
             sequence.Where(predicate).FirstOrNone();
 
+        /// <summary>
+        /// Gets the last item in a sequence wrapped in an Option
+        /// </summary>
+        /// <returns>
+        /// <c>Some</c> with the last item of the sequence if the sequence has at least one item in it. <br/>
+        /// <c>None</c> if the sequence is empty.
+        /// </returns>
         public static Option<T> LastOrNone<T>(this IEnumerable<T> sequence) =>
             sequence.Select(x => (Option<T>)new Some<T>(x))
                 .DefaultIfEmpty(None.Value)
                 .Last();
 
+        /// <summary>
+        /// Gets the last item in a sequence that matches the supplied predicate, wrapped in an Option
+        /// </summary>
+        /// <param name="predicate">The predicate to filter the list with.</param>
+        /// <returns>
+        /// <c>Some</c> with the last item of the sequence if the sequence has at least one item in it that matches the predicate. <br/>
+        /// <c>None</c> if no items in the sequence match the predicate.
+        /// </returns>
         public static Option<T> LastOrNone<T>(
             this IEnumerable<T> sequence,
             Func<T, bool> predicate) =>
             sequence.Where(predicate).LastOrNone();
 
+        /// <summary>
+        /// Gets the contents of all <c>Some</c>'s in the sequence
+        /// </summary>
+        /// <returns>
+        /// A sequence the Contents of all the <c>Some</c>'s in the original sequence.
+        /// </returns>
         public static IEnumerable<T> SelectOptional<T>(
             this IEnumerable<Option<T>> sequence) =>
             sequence
                 .OfType<Some<T>>()
                 .Select(s => s.Content);
 
+        /// <summary>
+        /// Maps the sequence to a new sequence with the passed map function and returns the contents of all <c>Some</c>'s in the sequence
+        /// </summary>
+        /// <param name="map">The function to map over the list with.</param>
+        /// <returns>
+        /// A sequence the Contents of all the <c>Some</c>'s in the resulting mapped sequence.
+        /// </returns>
         public static IEnumerable<TResult> SelectOptional<T, TResult>(
             this IEnumerable<T> sequence, Func<T, Option<TResult>> map) =>
             sequence.Select(map)
--- FuSEx.Option/Option.cs
+++ FuSEx.Option/Option.cs
@@ -10,11 +10,57 @@ namespace FuSEx.Option
         public static implicit operator Option<T>(None none) =>
             new None<T>();
 
+        /// <summary>
+        /// Maps <c>Option{T}</c> to <c>Option{TResult}</c> using the supplied function.
+        /// </summary>
+        /// <param name="map">A map function to transform the <c>Option{T}</c> to an <c>Option{TResult}</c>.</param>
+        /// <returns>
+        /// <c>Some{TResult}</c> if the <c>Option{T}</c> was originally a <c>Some{T}</c>. <br/>
+        /// <c>None{TResult}</c> if the <c>Option{T}</c> was originally a None{T}</c>.
+        /// </returns>
+        /// <remarks>The map function is not evaluated if this <c>Option{T}</c> is a <c>None{T}</c>.</remarks>
         public abstract Option<TResult> Map<TResult>(Func<T, TResult> map);
+
+        /// <summary>
+        /// Maps <c>Option{T}</c> to <c>Option{TResult}</c> using the supplied function.
+        /// </summary>
+        /// <param name="map">A map function to transform the <c>Option{T}</c> to an <c>Option{TResult}</c>.</param>
+        /// <returns>
+        /// <c>Some{TResult}</c> if the <c>Option{T}</c> was originally a <c>Some{T}</c>. <br/>
+        /// <c>None{TResult}</c> if the <c>Option{T}</c> was originally a None{T}</c>.
+        /// </returns>
+        /// <remarks>The difference between this and Map is that the supplied map function returns an Option, use this to prevent nested Options.</remarks>
+        /// <see cref="Map(Func{T, TResult})">
         public abstract Option<TResult> MapOptional<TResult>(Func<T, Option<TResult>> map);
+
+        /// <summary>
+        /// Reduces <c>Option{T}</c> to <c>T</c> using either <c>Some{T}.Content</c> or a default value supplied as a parameter.
+        /// </summary>
+        /// <param name="whenNone">The default value returned if this Option is <c>None{T}</c>.</param>
+        /// <returns>
+        /// <c>Some{TResult}.Content</c> if this <c>Option{T}</c> was originally a <c>Some{T}</c>. <br/>
+        /// The supplied value for  <c>whenNone</c> if this <c>Option{T}</c> was originally a None{T}</c>.
+        /// </returns>
         public abstract T Reduce(T whenNone);
+
+        /// <summary>
+        /// Reduces <c>Option{T}</c> to <c>T</c> using either <c>Some{T}.Content</c> or evaluates the supplied function which will return the value.
+        /// </summary>
+        /// <param name="whenNone">The default value returned if this Option is <c>None{T}</c>.</param>
+        /// <returns>
+        /// <c>Some{TResult}.Content</c> if this <c>Option{T}</c> was originally a <c>Some{T}</c>. <br/>
+        /// The result of the supplied function if this <c>Option{T}</c> was originally a None{T}</c>.
+        /// </returns>
+        /// <remarks>The passed function is not evaluated if this <c>Option{T}</c> is a <c>None{T}</c>.</remarks>
         public abstract T Reduce(Func<T> whenNone);
 
+        /// <summary>
+        /// Casts a <c>Option{T}</c> to an </c>Option{TNew}</c> 
+        /// </summary>
+        /// <returns>
+        /// <c>Some{TNew}</c> if this Option is a <c>Some</c> and is castable to <c>Some{TNew}</c>. <br/>
+        /// <c>None{TNew}</c> if this Option is a <c>None</c> or is not castable to <c>Some{TNew}</c>.
+        /// </returns>
         public Option<TNew> OfType<TNew>() where TNew : class =>
             this is Some<T> some && typeof(TNew).IsAssignableFrom(typeof(T))
                 ? (Option<TNew>)new Some<TNew>(some.Content as TNew)
